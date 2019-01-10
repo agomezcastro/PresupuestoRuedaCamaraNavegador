5d504e00881e4d2817033e1f31e3ab449ca4ff25
agomezcastro <agomezcastro@danielcastelao.org>
2019-01-10 19:45:00 +0100

    lee y muestra fichero

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#file").change(function (e) {
+        let reader = new FileReader();
+        reader.readAsDataURL(e.target.files[0]);
+
+        reader.onload = function () {
+            $("#imagen").attr("src", reader.result);
+        }
+    });
+});
